37b29f2a607ddbf70d50bfac9ccd320c93b177e6
GovoruhinAA <GovoruhinAA@npoa.org>
2020-05-21 12:31:54 +0500

    Add Empty MPI Project

---
+++ samples/B.MPI/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using EquipApps.WorkBench;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace B.MPI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : WbApp
+    {
+    }
+}
